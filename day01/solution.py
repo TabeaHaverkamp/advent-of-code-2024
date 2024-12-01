1632b41525e59939ab472e77f29f0bb94c279731
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-01 10:03:39 +0100

    more efficient

--- day01/solution.py
+++ day01/solution.py
@@ -1,30 +1,18 @@
 
-with open('input.txt', 'r') as file:
-    lines = [line.strip().split("   ") for line in file]
-    lines1 = [int(l[0]) for l in lines]
-    lines2 = [int(l[1]) for l in lines]
-        
+def parse_input(file_path):
+    with open(file_path, 'r') as file:
+        data = [list(map(int, line.strip().split("   "))) for line in file]
+    return [d[0] for d in data], [d[1] for d in data]
 
 def distance(lines1, lines2):
-    lines1= sorted(lines1) 
-    lines2 = sorted(lines2)
-    print(lines1)
-    print(lines2)
-    dist = 0
-    for idx, _ in enumerate(lines1):
-        a = abs(lines1[idx]-lines2[idx])
-        dist += a
-    print('---')
+    lines1, lines2 = sorted(lines1), sorted(lines2)
+    dist = sum(abs(a - b) for a, b in zip(lines1, lines2))
     print(dist)
 
-distance(lines1, lines2)
 
 def similarity(lines1, lines2):
-    sim = 0
-    for i in lines1:
-        cnt =lines2.count(i)
-        sim += i *cnt
-    print('----')
+    sim = sum(x * lines2.count(x) for x in lines1)
     print(sim)
-        
+lines1, lines2 = parse_input('testinput.txt')
+distance(lines1, lines2)      
 similarity(lines1, lines2)
