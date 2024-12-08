2352d8aad5c80f6e8c4d31c2193680d502b85e19
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-08 08:58:29 +0100

    day 08 part 2

--- day08/solution.py
+++ day08/solution.py
@@ -9,7 +9,7 @@ def parse_input(file_path):
     return data
 
 
-def solution1(data):
+def solution(data, solution_part=1):
     elems = set(functools.reduce(operator.iconcat, data, []))
     elems.remove(".")
     antinodes = set()
@@ -29,22 +29,74 @@ def solution1(data):
             if c1_x and c2_x:
                 antinodes.add((c1[0] - dist_x, c1[1] - dist_y))
                 antinodes.add((c2[0] + dist_x, c2[1] + dist_y))
+                if solution_part == 2:
+                    i = 2
+                    while c1[0] - i * dist_x >= 0 and c1[1] - i * dist_y >= 0:
+                        antinodes.add((c1[0] - i * dist_x, c1[1] - i * dist_y))
+                        i += 1
+                    i = 2
+                    while c2[0] + i * dist_x < len(data) and c2[1] + i * dist_y < len(
+                        data
+                    ):
+                        antinodes.add((c2[0] + i * dist_x, c2[1] + i * dist_y))
+                        i += 1
             if not c1_x and not c2_x:
                 antinodes.add((c2[0] - dist_x, c2[1] - dist_y))
                 antinodes.add((c1[0] + dist_x, c1[1] + dist_y))
-
+                if solution_part == 2:
+                    i = 2
+                    while c2[0] - i * dist_x >= 0 and c2[1] - i * dist_y >= 0:
+                        antinodes.add((c2[0] - i * dist_x, c2[1] - i * dist_y))
+                        i += 1
+                    i = 2
+                    while c1[0] + i * dist_x < len(data) and c1[1] + i * dist_y < len(
+                        data
+                    ):
+                        antinodes.add((c1[0] + i * dist_x, c1[1] + i * dist_y))
+                        i += 1
             if c1_x and not c2_x:
                 antinodes.add((c1[0] - dist_x, c1[1] + dist_y))
                 antinodes.add((c2[0] + dist_x, c2[1] - dist_y))
+                if solution_part == 2:
+                    i = 2
+                    while c1[0] - i * dist_x >= 0 and c1[1] + i * dist_y >= 0:
+                        antinodes.add((c1[0] - i * dist_x, c1[1] + i * dist_y))
+                        i += 1
+                    i = 2
+                    while c2[0] + i * dist_x < len(data) and c2[1] - i * dist_y < len(
+                        data
+                    ):
+                        antinodes.add((c2[0] + i * dist_x, c2[1] - i * dist_y))
+                        i += 1
             if (not c1_x) and c2_x:
                 antinodes.add((c1[0] + dist_x, c1[1] - dist_y))
                 antinodes.add((c2[0] - dist_x, c2[1] + dist_y))
+                if solution_part == 2:
+                    i = 2
+                    while c1[0] + i * dist_x >= 0 and c1[1] - i * dist_y >= 0:
+                        antinodes.add((c1[0] + i * dist_x, c1[1] - i * dist_y))
+                        i += 1
+                    i = 2
+                    while c2[0] - i * dist_x < len(data) and c2[1] + i * dist_y < len(
+                        data
+                    ):
+                        antinodes.add((c2[0] - i * dist_x, c2[1] + i * dist_y))
+                        i += 1
+            if solution_part == 2:
+                antinodes.add(c1)
+                antinodes.add(c2)
 
     # remove antinodes outside of grid
     antinodes = {tup for tup in antinodes if all(0 <= x < len(data) for x in tup)}
+    for elem in antinodes:
+        if data[elem[0]][elem[1]] == ".":
+            data[elem[0]][elem[1]] = "#"
+    for r in data:
+        print("".join(r))
+    print("---------")
 
-    print("solution 1: ", len(antinodes))
+    print(f"solution {solution_part}: {len(antinodes)}")
 
 
 data = parse_input("input.txt")
-solution1(data)
+solution(data, solution_part=2)
