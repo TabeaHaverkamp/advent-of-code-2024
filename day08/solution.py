892a24b35d4807961b1ba8bb516c4bf4d6d5ce25
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-08 09:27:48 +0100

    actually using vectors

--- day08/solution.py
+++ day08/solution.py
@@ -23,73 +23,21 @@ def solution(data, solution_part=1):
         ]
         combinations = list(itertools.combinations(occ, 2))
         for c1, c2 in combinations:
-            dist_x = abs(c1[0] - c2[0])
-            dist_y = abs(c1[1] - c2[1])
-            c1_x = c1[0] < c2[0]
-            c2_x = c1[1] < c2[1]
-            if c1_x and c2_x:
-                if solution_part == 2:
-                    i = 0
-                    while c1[0] - i * dist_x >= 0 and c1[1] - i * dist_y >= 0:
-                        antinodes.add((c1[0] - i * dist_x, c1[1] - i * dist_y))
-                        i += 1
-                    i = 0
-                    while c2[0] + i * dist_x < len(data) and c2[1] + i * dist_y < len(
-                        data
-                    ):
-                        antinodes.add((c2[0] + i * dist_x, c2[1] + i * dist_y))
-                        i += 1
-                elif solution_part == 1:
-                    antinodes.add((c1[0] - dist_x, c1[1] - dist_y))
-                    antinodes.add((c2[0] + dist_x, c2[1] + dist_y))
+            dist_x = c1[0] - c2[0]
+            dist_y = c1[1] - c2[1]
 
-            if not c1_x and not c2_x:
-                if solution_part == 2:
-                    i = 0
-                    while c2[0] - i * dist_x >= 0 and c2[1] - i * dist_y >= 0:
-                        antinodes.add((c2[0] - i * dist_x, c2[1] - i * dist_y))
-                        i += 1
-                    i = 0
-                    while c1[0] + i * dist_x < len(data) and c1[1] + i * dist_y < len(
-                        data
-                    ):
-                        antinodes.add((c1[0] + i * dist_x, c1[1] + i * dist_y))
-                        i += 1
-                elif solution_part == 1:
-                    antinodes.add((c2[0] - dist_x, c2[1] - dist_y))
-                    antinodes.add((c1[0] + dist_x, c1[1] + dist_y))
-
-            if c1_x and not c2_x:
-                if solution_part == 2:
-                    i = 0
-                    while c1[0] - i * dist_x >= 0 and c1[1] + i * dist_y >= 0:
-                        antinodes.add((c1[0] - i * dist_x, c1[1] + i * dist_y))
-                        i += 1
-                    i = 0
-                    while c2[0] + i * dist_x < len(data) and c2[1] - i * dist_y < len(
-                        data
-                    ):
-                        antinodes.add((c2[0] + i * dist_x, c2[1] - i * dist_y))
-                        i += 1
-                elif solution_part == 1:
-                    antinodes.add((c1[0] - dist_x, c1[1] + dist_y))
-                    antinodes.add((c2[0] + dist_x, c2[1] - dist_y))
-
-            if (not c1_x) and c2_x:
-                if solution_part == 2:
-                    i = 0
-                    while c1[0] + i * dist_x >= 0 and c1[1] - i * dist_y >= 0:
-                        antinodes.add((c1[0] + i * dist_x, c1[1] - i * dist_y))
-                        i += 1
-                    i = 0
-                    while c2[0] - i * dist_x < len(data) and c2[1] + i * dist_y < len(
-                        data
-                    ):
-                        antinodes.add((c2[0] - i * dist_x, c2[1] + i * dist_y))
-                        i += 1
-                elif solution_part == 1:
-                    antinodes.add((c1[0] + dist_x, c1[1] - dist_y))
-                    antinodes.add((c2[0] - dist_x, c2[1] + dist_y))
+            if solution_part == 1:
+                antinodes.add((c1[0] + dist_x, c1[1] + dist_y))
+                antinodes.add((c2[0] - dist_x, c2[1] - dist_y))
+            elif solution_part == 2:
+                i = 0
+                while c1[0] + i * dist_x >= 0 and c1[1] + i * dist_y >= 0:
+                    antinodes.add((c1[0] + i * dist_x, c1[1] + i * dist_y))
+                    i += 1
+                i = 0
+                while c2[0] - i * dist_x < len(data) and c2[1] - i * dist_y < len(data):
+                    antinodes.add((c2[0] - i * dist_x, c2[1] - i * dist_y))
+                    i += 1
 
     # remove antinodes outside of grid
     antinodes = {tup for tup in antinodes if all(0 <= x < len(data) for x in tup)}
