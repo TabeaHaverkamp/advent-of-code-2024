26717d6191bbd9af30927719550a833e1a53d056
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-11 15:32:49 +0100

    more efficient solution for both parts

--- day11/solution.py
+++ day11/solution.py
@@ -8,33 +8,9 @@ def parse_input(file_path):
 
 
 def solution(data, r=25):
-    for _ in range(r):
-        idx = 0
-        l = len(data)
-        while idx < l:
-            elem = data[idx]
-            if elem == 0:
-                data[idx] = 1
-                idx += 1
-            elif len(str(elem)) % 2 == 0:
-                s_e = str(elem)
-                first = int(s_e[: int(len(s_e) / 2)])
-                second = int(s_e[int(len(s_e) / 2) :])
-                data[idx] = first
-                data.append(second)
-                idx += 1
-            else:
-                data[idx] = elem * 2024
-                idx += 1
-
-    return len(data)
-
-
-def solution2(data, r=25):
     m = {d: 1 for d in data}
     for _ in range(r):
         a = {}
-
         for k, how_often in list(m.items()):
             if k == 0:
                 a[1] = a.get(1, 0) + how_often
@@ -65,5 +41,5 @@ print(f"solution 1: {sol1} (runtime: {(time.time() - start_time)} seconds)")
 
 
 start_time = time.time()
-sol2 = solution2(data, 75)
+sol2 = solution(data, 75)
 print(f"solution 2: {sol2} (runtime: {(time.time() - start_time)} seconds)")
