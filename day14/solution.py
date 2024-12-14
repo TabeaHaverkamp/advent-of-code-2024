231b5932ba21b38399b86959db786c0f50a903cd
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-14 18:43:07 +0100

    day 14 part 2

--- day14/solution.py
+++ day14/solution.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import time
+import re
 
 
 def parse_input(file_path):
@@ -51,8 +52,56 @@ def solution1(data, test_input=False):
     return quadrant_sum
 
 
+def solution2(data, test_input=False, print_tree=False):
+
+    if test_input:
+        size = (7, 11)
+    else:
+        size = (103, 101)
+    found_christmas_tree = False
+    i = 1
+    while not found_christmas_tree:
+        result = {}
+
+        res = [["." for _ in range(size[1])] for _ in range(size[0])]
+        for idx, robot in enumerate(data):
+
+            pos_y, pos_x = robot[0]
+            vel_y, vel_x = robot[1]
+            pos_x = (pos_x + vel_x) % size[0]
+            pos_y = (pos_y + vel_y) % size[1]
+
+            result[(pos_x, pos_y)] = result.get((pos_x, pos_y), 0) + 1
+
+            data[idx] = [(pos_y, pos_x), robot[1]]
+
+        # build the display
+        for k in result.keys():
+            res[k[0]][k[1]] = "X"
+
+        pattern = "XXXXXXXXXXXXXXXXX"
+
+        if sum(pattern in "".join(s) for s in res) >= 3:
+            if print_tree:
+                print("-------", i, "-----")
+                for l in res:
+                    print("".join(l), pattern in "".join(l))
+            found_christmas_tree = True
+        if i >= 10000:
+            print("end of loop!")
+            found_christmas_tree = True
+        i += 1
+
+    return i - 1
+
+
 data = parse_input("input.txt")
 
 start_time = time.time()
 sol1 = solution1(data, test_input=False)
 print(f"solution 1: {sol1} (runtime: {(time.time() - start_time)} seconds)")
+
+
+start_time = time.time()
+sol2 = solution2(data, test_input=False, print_tree=False)
+print(f"solution 2: {sol2} (runtime: {(time.time() - start_time)} seconds)")
