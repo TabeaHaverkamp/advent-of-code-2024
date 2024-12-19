d5ddffbc44c0d4838a9e6867fcf1e45599660a65
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-19 09:21:14 +0100

    improving readability

--- day19/solution.py
+++ day19/solution.py
@@ -19,61 +19,46 @@ def parse_input(file_path):
     return towels, displays
 
 
+def word_construction_problem(towels, target):
+    """
+    Implements the word construction problem, calculating how many possible solutions
+    there are to build the target string.
+    Has an array (dp) to store integers, where dp[i] represents the number of
+    ways the substring target[:i] can be formed using the given strings."""
+    # initialise that every substring cannot be constructed
+    dp = [0] * (len(target) + 1)
+    dp[0] = 1  # Base case: empty string can be formed
+
+    # go through the target string one by one
+    for i in range(1, len(target) + 1):
+        # check every given towel to build from
+        for word in towels:
+            # if the towel would fit into the substring and the last len(towel) characters
+            # of the substring are equal to the towel
+            # then set this part of the solution can be build (if the substring before towel can be build)
+            if i >= len(word) and target[i - len(word) : i] == word:
+                # on spot i, there are now + many solutions as in the
+                # array before the word began
+                dp[i] += dp[i - len(word)]
+
+    return dp[-1]
+
+
 def solution1(towels, displays):
-    def word_construction(towels, target):
-        """
-        Implements the word construction problem.
-        """
-        # initialise that every substring cannot be constructed
-        dp = [False] * (len(target) + 1)
-        dp[0] = True  # Base case: empty string can be formed
-
-        # go through the target string one by one
-        for i in range(1, len(target) + 1):
-            # check every given towel to build from
-            for word in towels:
-                # if the towel would fit into the substring and the last len(towel) characters
-                # of the substring are equal to the towel
-                # then set this part of the solution can be build (if the substring before towel can be build)
-                if i >= len(word) and target[i - len(word) : i] == word:
-                    dp[i] = dp[i] or dp[i - len(word)]
-
-        return dp[-1]
 
     possible_combinations = 0
     for display in displays:
-        possible = word_construction(towels, display)
-        possible_combinations += int(possible)
-
+        possible = word_construction_problem(towels, display)
+        possible_combinations += int(possible > 0)
     return possible_combinations
 
 
 def solution2(towels, displays):
-    def word_construction_problem(towels, target):
-        """Instead of using a dp array to store booleans (True or False),
-        we use a dp array to store integers, where dp[i] represents the number of
-        ways the substring target[:i] can be formed using the given strings."""
-        # initialise that every substring cannot be constructed
-        dp = [0] * (len(target) + 1)
-        dp[0] = 1  # Base case: empty string can be formed
-
-        # go through the target string one by one
-        for i in range(1, len(target) + 1):
-            # check every given towel to build from
-            for word in towels:
-                # if the towel would fit into the substring and the last len(towel) characters
-                # of the substring are equal to the towel
-                # then set this part of the solution can be build (if the substring before towel can be build)
-                if i >= len(word) and target[i - len(word) : i] == word:
-                    dp[i] += dp[i - len(word)]
-
-        return dp[-1]
 
     possible_combinations = 0
     for display in displays:
         possible = word_construction_problem(towels, display)
         possible_combinations += int(possible)
-
     return possible_combinations
 
 
