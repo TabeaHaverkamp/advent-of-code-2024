2ea7a915dcf566230a1bec21c33275b3cff1d009
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-19 09:17:41 +0100

    day 19 part 2

--- day19/solution.py
+++ day19/solution.py
@@ -48,8 +48,41 @@ def solution1(towels, displays):
     return possible_combinations
 
 
+def solution2(towels, displays):
+    def word_construction_problem(towels, target):
+        """Instead of using a dp array to store booleans (True or False),
+        we use a dp array to store integers, where dp[i] represents the number of
+        ways the substring target[:i] can be formed using the given strings."""
+        # initialise that every substring cannot be constructed
+        dp = [0] * (len(target) + 1)
+        dp[0] = 1  # Base case: empty string can be formed
+
+        # go through the target string one by one
+        for i in range(1, len(target) + 1):
+            # check every given towel to build from
+            for word in towels:
+                # if the towel would fit into the substring and the last len(towel) characters
+                # of the substring are equal to the towel
+                # then set this part of the solution can be build (if the substring before towel can be build)
+                if i >= len(word) and target[i - len(word) : i] == word:
+                    dp[i] += dp[i - len(word)]
+
+        return dp[-1]
+
+    possible_combinations = 0
+    for display in displays:
+        possible = word_construction_problem(towels, display)
+        possible_combinations += int(possible)
+
+    return possible_combinations
+
+
 towels, displays = parse_input(filename)
 
 start_time = time.time()
 sol1 = solution1(towels, displays)
 print(f"solution 1: {sol1} (runtime: {(time.time() - start_time)} seconds)")
+
+start_time = time.time()
+sol2 = solution2(towels, displays)
+print(f"solution 2: {sol2} (runtime: {(time.time() - start_time)} seconds)")
