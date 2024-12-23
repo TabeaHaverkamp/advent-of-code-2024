9311309147d1a5075844c4efa5ac66acd4af4fc0
Tabea Haverkamp <tabea.haverkamp@vimla.se>
2024-12-23 10:03:49 +0100

    day 23 part 2

--- day23/solution.py
+++ day23/solution.py
@@ -1,6 +1,6 @@
 import time
 import networkx as nx
-
+from collections import defaultdict
 
 TEST_INPUT = False
 if TEST_INPUT:
@@ -28,9 +28,23 @@ def solution1(edges):
     return len(cycles)
 
 
+def solution2(edges):
+
+    G = nx.DiGraph(edges).to_undirected()
+    cliques = list(nx.find_cliques(G))
+    biggest_clique = max(cliques, key=len)
+    password = ",".join(sorted(biggest_clique))
+    return password
+
+
 paths = parse_input(filename)
 
 
 start_time = time.time()
 sol1 = solution1(paths)
 print(f"solution 1: {sol1} (runtime: {(time.time() - start_time)} seconds)")
+
+
+start_time = time.time()
+sol2 = solution2(paths)
+print(f"solution 2: {sol2} (runtime: {(time.time() - start_time)} seconds)")
